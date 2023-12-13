22ca3371a97691068418ed643c5681dea8ff8a5e
zgood22 <148004917+zgood22@users.noreply.github.com>
2023-12-13 15:06:59 -0500

    Bootstrap google (#2)
    * Added book_query, pull_book, ss
    
    * port-over now runs on flask
    
    * app runs with google Oauth
    
    * adjustments need to fix ss.py
    
    * Includes google profile image
    
    * Added comment to defunct bootstrap for programmers

---
+++ app/ss.py
@@ -0,0 +1,168 @@
+# adapted from:
+# ... https://developers.google.com/sheets/api/guides/authorizing
+# ... https://gspread.readthedocs.io/en/latest/oauth2.html
+# ... https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
+# ... https://github.com/s2t2/birthday-wishes-py/blob/master/app/sheets.py
+# ... https://raw.githubusercontent.com/prof-rossetti/flask-sheets-template-2020/master/web_app/spreadsheet_service.py
+
+import os
+from datetime import datetime, timezone
+from pprint import pprint
+
+from dotenv import load_dotenv
+import gspread
+from gspread.exceptions import SpreadsheetNotFound
+
+
+load_dotenv()
+
+DEFAULT_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "google-credentials.json")
+GOOGLE_CREDENTIALS_FILEPATH = os.getenv("GOOGLE_CREDENTIALS_FILEPATH", default=DEFAULT_FILEPATH)
+
+GOOGLE_SHEETS_DOCUMENT_ID = os.getenv("GOOGLE_SHEETS_DOCUMENT_ID", default="OOPS Please get the spreadsheet identifier from its URL, and set the 'GOOGLE_SHEETS_DOCUMENT_ID' environment variable accordingly...")
+
+
+class SpreadsheetService:
+    # TODO: consider implementing a locking mechanism on sheet writes, to prevent overwriting (if it becomes an issue)
+    # ... however we know that if we want a more serious database solution, we would choose SQL database (and this app is just a small scale demo)
+
+    def __init__(self, credentials_filepath=GOOGLE_CREDENTIALS_FILEPATH, document_id=GOOGLE_SHEETS_DOCUMENT_ID):
+        print("INITIALIZING NEW SPREADSHEET SERVICE...")
+
+        #self.credentials = credentials or ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
+        #self.credentials = ServiceAccountCredentials._from_parsed_json_keyfile(json.loads(GOOGLE_API_CREDENTIALS), AUTH_SCOPE)
+        #self.client = gspread.authorize(self.credentials) #> <class 'gspread.client.Client'>
+        self.client = gspread.service_account(filename=credentials_filepath)
+
+        self.document_id = document_id
+
+        # TODO: consider storing and caching the latest version of each sheet, given the small number of sheets
+        # ... showing preference for speed over brevity
+        #self.products_sheet = None
+        #self.orders_sheet = None
+
+    @property
+    def doc(self):
+        """note: this will make an API call each time, to get the new data"""
+        return self.client.open_by_key(self.document_id) #> <class 'gspread.models.Spreadsheet'>
+
+    def get_sheet(self, sheet_name):
+        return self.doc.worksheet(sheet_name)
+
+    @staticmethod
+    def generate_timestamp():
+        return datetime.now(tz=timezone.utc)
+
+    @staticmethod
+    def parse_timestamp(ts:str):
+        """
+            ts (str) : a timestamp string like '2023-03-08 19:59:16.471152+00:00'
+        """
+        date_format = "%Y-%m-%d %H:%M:%S.%f%z"
+        return datetime.strptime(ts, date_format)
+
+    
+    def get_records(self, sheet_name):
+        """Gets all records from a sheet,
+            converts datetime columns back to Python datetime objects
+        """
+    
+
+        #print(f"GETTING RECORDS FROM SHEET: '{sheet_name}'")
+        sheet = self.get_sheet(sheet_name) #> <class 'gspread.models.Worksheet'>
+        records = sheet.get_all_records() #> <class 'list'>
+        for record in records:
+            if record.get("created_at"):
+                record["created_at"] = self.parse_timestamp(record["created_at"])
+        return sheet, records
+    
+    def create_records(self, sheet_name: str, new_record: dict):
+        model_class = {"products": Product, "orders": Order, "books": Book}[sheet_name]
+
+        sheet, records = self.get_records(sheet_name)
+        next_row_number = len(records) + 2  # plus headers plus one
+
+        # auto-increment integer identifier
+        if any(records):
+            existing_ids = [r["id"] for r in records]
+            next_id = max(existing_ids) + 1
+        else:
+            next_id = 1
+
+        
+        
+
+
+        # Create a new dictionary with the updated values
+        updated_record = {
+            "id": next_id,
+            "title": new_record["title"],
+            "author": new_record["author"],
+            "published_date": new_record["published_date"],
+            "condition": new_record["condition"],
+            "list_price": new_record["list_price"],
+            "image_url": new_record["image_url"],
+            "created_at": '2023-03-08 19:59:16.471152+00:00',
+        }
+
+        new_row = model_class(updated_record).to_row
+
+        sheet.insert_rows([new_row], row=next_row_number)
+
+
+    
+
+
+
+
+
+# FIXED SCHEMA / DECORATORS
+# ... to make sure when writing to sheet the values are in the proper order
+
+class Product:
+    def __init__(self, attrs):
+        self.id = attrs.get("id")
+        self.name = attrs.get("name")
+        self.description = attrs.get("description")
+        self.price = attrs.get("price")
+        self.url = attrs.get("url")
+        self.created_at = attrs.get("created_at")
+
+    @property
+    def to_row(self):
+        return [self.id, self.name, self.description, self.price, self.url, str(self.created_at)]
+
+
+class Order:
+    def __init__(self, attrs):
+        self.id = attrs.get("id")
+        self.user_email = attrs.get("user_email")
+        self.product_id = attrs.get("product_id")
+        self.product_name = attrs.get("product_name")
+        self.product_price = attrs.get("product_price")
+        self.created_at = attrs.get("created_at")
+
+
+    @property
+    def to_row(self):
+        return [self.id, self.user_email, self.product_id, self.product_name, self.product_price, str(self.created_at)]
+
+
+class Book:
+    def __init__(self, attrs):
+        self.id = attrs.get("id")
+        self.title = attrs.get("title")
+        self.author = attrs.get("author")
+        self.published_date = attrs.get("published_date")
+        self.condition = attrs.get("condition")
+        self.list_price = attrs.get("list_price")
+        self.image_url = attrs.get("image_url")
+        self.created_at = attrs.get("created_at")
+        
+        
+        
+
+
+    @property
+    def to_row(self):
+        return[self.id, self.title, self.author, self.published_date, self.condition, self.list_price, self.image_url, self.created_at]
---
+++ web_app/routes/book_routes.py
@@ -0,0 +1,72 @@
+from flask import Blueprint, request, render_template
+from app.book_query import get_book_details
+from app.ss import SpreadsheetService
+
+
+
+#from web_app.book_routes import listing_search
+
+from app.ss import SpreadsheetService
+
+
+
+book_routes = Blueprint("book_routes", __name__)
+
+
+# Assuming you have an existing Blueprint named 'book_routes'
+# If not, replace 'book_routes' with your Blueprint name or app
+
+@book_routes.route('/new-listing')
+def new_listing():
+    return render_template('book_form.html')
+
+@book_routes.route('/listing-search', methods=["GET", "POST"])
+def listing_search():
+    if request.method == "POST":
+        # for data sent via POST request, form inputs are in request.form:
+        request_data = dict(request.form)
+        print("FORM DATA:", request_data)
+    else:
+        # for data sent via GET request, url params are in request.args
+        request_data = dict(request.args)
+        print("URL PARAMS:", request_data)
+
+    author_fname = str(request_data.get("authorFirstName"))
+    author_lname = str(request_data.get("authorLastName"))
+    author_fullname = author_fname + " " + author_lname
+    condition = request_data.get("condition")
+    listPrice = request_data.get("listPrice")
+    book_title = str(request_data.get("title"))
+    book_results = get_book_details(book_title, condition, listPrice)
+    # ... existing code ...
+    print(condition, listPrice)
+    return render_template('listing_search.html', book_title=book_title, author_fullname=author_fullname, condition=condition, listPrice=listPrice, book_results=book_results)
+
+
+@book_routes.route('/selected-book', methods=["GET", "POST"])
+def selected_book():
+    if request.method == "POST":
+        # for data sent via POST request, form inputs are in request.form:
+        request_data = dict(request.form)
+        print("FORM DATA:", request_data)
+    else:
+        # for data sent via GET request, url params are in request.args
+        request_data = dict(request.args)
+        print("URL PARAMS:", request_data)
+
+    book_details = {
+        'title': request.form.get('title'),
+        'author': request.form.get('author'),
+        'published_date': request.form.get('published_date'),
+        'image_url': request.form.get('image_url'),
+        'condition': request.form.get('condition'),
+        'listPrice': request.form.get('listPrice'),
+        'created_at': request.form.get('created_at'),
+    }
+    
+
+
+    ss = SpreadsheetService()
+    print("Book Details:", book_details)
+    ss.create_records("books", book_details)
+    return render_template('selected_book.html', book_details=book_details)
