069453afbedf0e721545cc6d47a3653390dc523e
jlm456 <jlm456@georgetown.edu>
2023-12-13 14:31:11 -0500

    Gmail login working

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -81,8 +81,8 @@ def create_app(spreadsheet_service=None):
         name="google",
         server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
         client_kwargs={"scope": "openid email profile"},
-        client_id=GOOGLE_CLIENT_ID,
-        client_secret=GOOGLE_CLIENT_SECRET,
+        client_id=GOOGLE_CLIENT_ID1,
+        client_secret=GOOGLE_CLIENT_SECRET1,
         #authorize_params={"access_type": "offline"} # give us the refresh token! see: https://stackoverflow.com/questions/62293888/obtaining-and-storing-refresh-token-using-authlib-with-flask
     )
     app.config["OAUTH"] = oauth
