bbed51f377dd257d81b13aed04eade1bbfc5d9e9
jlm456 <jlm456@georgetown.edu>
2023-12-15 12:23:53 -0500

    testing progress

--- test/test_web_app.py
+++ test/test_web_app.py
@@ -8,23 +8,15 @@ def test_home_page(test_client):
     assert b"Books For Sale:" in response.data
     assert b"Account Page" in response.data
 
-def test_about_page(test_client):
-    response = test_client.get("/about")
-    assert response.status_code == 200
-    assert b"Welcome to Our About Page!" in response.data
-    assert b"Thank you for choosing Georgetown Book Exchange." in response.data
-    assert b"Georgetown Book Exchange is more than just a platform" in response.data
-    assert b"About the Creators!" in response.data
-    assert b"Samantha Miller is a senior at Georgetown University" in response.data
-    assert b"Zachary Goodman is a senior at Georgetown University" in response.data
+#def test_about_page(test_client):
+ #   response = test_client.get("/about")
+  #  assert response.status_code == 200
+   # assert b"Welcome to Our About Page!" in response.data
+   # assert b"Thank you for choosing Georgetown Book Exchange." in response.data
+   # assert b"Georgetown Book Exchange is more than just a platform" in response.data
+   # assert b"About the Creators!" in response.data
+   # assert b"Samantha Miller is a senior at Georgetown University" in response.data
+   # assert b"Zachary Goodman is a senior at Georgetown University" in response.data
 
 
-def test_products(test_client):
-    response = test_client.get("/products")
-    assert response.status_code == 200
-    assert b"<h1>Products</h1>" in response.data
 
-
-    assert b"Textbook" in response.data
-    assert b"Cup of Tea" in response.data
-    assert b"Strawberries" in response.data
