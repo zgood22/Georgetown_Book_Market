ca2d3509e954022ae19e777e0ac9911341d75f73
zgood22 <148004917+zgood22@users.noreply.github.com>
2023-12-15 10:39:54 -0500

    added testing for web_app

--- test/test_web_app.py
+++ test/test_web_app.py
@@ -2,15 +2,22 @@
 
 # tests for each page:
 
-def test_home(test_client):
+def test_home_page(test_client):
     response = test_client.get("/")
     assert response.status_code == 200
-    assert b"<h1>Home</h1>" in response.data
+    assert b"Books For Sale:" in response.data
+    assert b"Account Page" in response.data
 
-def test_about(test_client):
+def test_about_page(test_client):
     response = test_client.get("/about")
     assert response.status_code == 200
-    assert b"<h1>About</h1>" in response.data
+    assert b"Welcome to Our About Page!" in response.data
+    assert b"Thank you for choosing Georgetown Book Exchange." in response.data
+    assert b"Georgetown Book Exchange is more than just a platform" in response.data
+    assert b"About the Creators!" in response.data
+    assert b"Samantha Miller is a senior at Georgetown University" in response.data
+    assert b"Zachary Goodman is a senior at Georgetown University" in response.data
+
 
 def test_products(test_client):
     response = test_client.get("/products")
