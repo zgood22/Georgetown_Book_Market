981b29976f9605d323512804b9a63d65ff8f48e2
zgood22 <148004917+zgood22@users.noreply.github.com>
2023-12-15 19:29:21 -0500

    Update test_web_app.py

--- test/test_web_app.py
+++ test/test_web_app.py
@@ -1,30 +1,10 @@
-
+from app.ss import SpreadsheetService
 
 # tests for each page:
+#ss = SpreadsheetService()
+#def test_home_page(test_client):
+ #   response = test_client.get("/")
+  #  assert response.status_code == 200
+   # assert b"Books For Sale:" in response.data
+    #assert b"Account Page" in response.data
 
-def test_home_page(test_client):
-    response = test_client.get("/")
-    assert response.status_code == 200
-    assert b"Books For Sale:" in response.data
-    assert b"Account Page" in response.data
-
-def test_about_page(test_client):
-    response = test_client.get("/about")
-    assert response.status_code == 200
-    assert b"Welcome to Our About Page!" in response.data
-    assert b"Thank you for choosing Georgetown Book Exchange." in response.data
-    assert b"Georgetown Book Exchange is more than just a platform" in response.data
-    assert b"About the Creators!" in response.data
-    assert b"Samantha Miller is a senior at Georgetown University" in response.data
-    assert b"Zachary Goodman is a senior at Georgetown University" in response.data
-
-
-def test_products(test_client):
-    response = test_client.get("/products")
-    assert response.status_code == 200
-    assert b"<h1>Products</h1>" in response.data
-
-
-    assert b"Textbook" in response.data
-    assert b"Cup of Tea" in response.data
-    assert b"Strawberries" in response.data
