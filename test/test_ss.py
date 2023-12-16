2b69d7e986bf49e54823f2edfd4e54da636c7734
jlm456 <jlm456@georgetown.edu>
2023-12-15 21:13:19 -0500

    update pytest

--- test/test_ss.py
+++ test/test_ss.py
@@ -1,21 +1,22 @@
 import os
 from datetime import datetime, timezone
-from pprint import pprint
+import pytest
 
-from flask import session
 
 from dotenv import load_dotenv
-import gspread
-from gspread.exceptions import SpreadsheetNotFound
+from gspread import Spreadsheet as Document, Worksheet
+
 from app.ss import SpreadsheetService
+
+
 
 load_dotenv()
 
 CI_ENV = (os.getenv("CI", default="false") == "true")
 CI_SKIP_MESSAGE = "taking a lighter touch to testing on the CI server, to reduce API usage and prevent rate limits"
 
-
 ss = SpreadsheetService()
+
 #
 def test_generate_timestamp():
     #dt = ss.generate_timestamp()
@@ -28,6 +29,8 @@ def test_generate_timestamp():
 
 
 def test_get_records():
+
+    
     sheet, records = ss.get_records("books")
     assert records[1]['genre'] == "Fiction"
 
